2e216b5b52f44563a754b22c6c23e17e6bb12df3
Behnam Nikkhah <bnikkhah22@gmail.com>
2017-10-29 15:11:14 -0600

    src fix

--- src/app/completed-projects.ts
+++ src/app/completed-projects.ts
@@ -4,46 +4,46 @@ export const PROJECTS: Project[] = [
 	{
 		name: 'Jammming',
 		url: 'http://jamming.surge.sh/',
-		image: '../assets/images/Jammming.png'
+		image: './assets/images/Jammming.png'
 	},
 	{
 		name: 'Ravenous',
 		url: 'http://ravenous2.surge.sh/',
-		image: '../assets/images/Ravenous.png'
+		image: './assets/images/Ravenous.png'
 	},
 	{
 		name: 'Jumpstart',
 		url: 'https://bnikkhah.github.io/Jumpstart/',
-		image: '../assets/images/Jumpstart.png'
+		image: './assets/images/Jumpstart.png'
 	},
 	{
 		name: 'Colmar Academy',
 		url: 'https://bnikkhah.github.io/ColmarAcademy/',
-		image: '../assets/images/ColmarAcademy.png'
+		image: './assets/images/ColmarAcademy.png'
 	},
 	{
 		name: 'Tea Cozy',
 		url: 'https://bnikkhah.github.io/teacozy/',
-		image: '../assets/images/TeaCozy.png'
+		image: './assets/images/TeaCozy.png'
 	},
 	{
 		name: 'Secret Agent Supply',
 		url: 'https://bnikkhah.github.io/SecretAgentSupply/',
-		image: '../assets/images/SecretAgentSupply.png'
+		image: './assets/images/SecretAgentSupply.png'
 	},
 	{
 		name: 'Turtoiseshell Optics',
 		url: 'https://bnikkhah.github.io/TortoiseshellOptics/',
-		image: '../assets/images/TortoiseshellOptics.png'
+		image: './assets/images/TortoiseshellOptics.png'
 	},
 	{
 		name: 'Junction',
 		url: 'https://bnikkhah.github.io/Junction/',
-		image: '../assets/images/Junction.png'
+		image: './assets/images/Junction.png'
 	},
 	{
 		name: 'Newsroom',
 		url: 'https://bnikkhah.github.io/Newsroom/',
-		image: '../assets/images/Newsroom.png'
+		image: './assets/images/Newsroom.png'
 	}
 ];
